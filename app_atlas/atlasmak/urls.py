aa9877824668c0b82c8de2dbfa2a7d820ff5f4f4
OrozomambetovaMeerim <meerimorozomambetova@gmail.com>
2022-06-17 22:21:01 +0600

    added APIViews and serializers

---
+++ app_atlas/atlasmak/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from atlasmak.views import *
+# from atlasmak.views import CompanyListAPIView, CertificateListAPIView
+
+
+
+urlpatterns = [
+    path('company/', CompanyListAPIView.as_view(), name='company-list'),
+    path('certificate/', CertificateListAPIView.as_view(), name='certificate-list'),
+    path('catalog/', CatalogListAPIView.as_view(), name='catalog-list'),
+    path('delivery/', DeliveryListAPIView.as_view(), name='delivery-list'),
+    path('visit/', VisitNumberListAPIView.as_view(), name='visit-list'),
+    path('day_number/', DayNumberListAPIView.as_view(), name='day_number-list'),
+]
--- app_atlas/atlasmak/views.py
+++ app_atlas/atlasmak/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import viewsets
+from rest_framework.response import Response
+from atlasmak.models import *
 
-# Create your views here.
+from atlasmak.serializers import CompanySerializer, CertificateSerializer, CatalogSerializer, DeliverySerializer, VisitNumberSerializer, DayNumberSerializer
+
+
+class CompanyListAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        company = Company.objects.all()
+        company_json = CompanySerializer(company, many=True)
+        return Response(data=company_json.data)
+
+
+class CertificateListAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        certificate = Certificate.objects.all()
+        certificate_json = CertificateSerializer(certificate, many=True)
+        return Response(data=certificate_json.data)
+
+
+class CatalogListAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        catalog = Catalog.objects.all()
+        catalog_json = CatalogSerializer(catalog, many=True)
+        return Response(data=catalog_json.data)
+
+
+class DeliveryListAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        delivery = Delivery.objects.all()
+        delivery_json = DeliverySerializer(delivery, many=True)
+        return Response(data=delivery_json.data)
+
+
+class VisitNumberListAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        visit_number = VisitNumber.objects.all()
+        visit_number_json = VisitNumberSerializer(visit_number, many=True)
+        return Response(data=visit_number_json.data) 
+
+
+class DayNumberListAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        day_number = DayNumber.objects.all()
+        day_number_json = DayNumberSerializer(day_number, many=True)
+        return Response(data=day_number_json.data) 
